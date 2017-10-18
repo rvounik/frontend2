4d85bdd01e6deda8bee3a85612fa22a8354e8829
Robbin van Ooij <robbin@vanooij.nl>
2017-10-18 11:14:30 +0200

    documented postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     plugins: [
+        // precss allows sass-like syntax
         require('precss'),
+        // autoprefixer ensures browser prefixes are added automatically
         require('autoprefixer')
     ]
-}
+};
+
